f063e02875f3bb27779610143a5100a92b1d8437
padmalatha <padmalatham1122@gmail.com>
2022-03-10 12:37:45 +0530

    First commit

---
+++ dbutils.java
@@ -0,0 +1,4 @@
+Hello guys
+Once you create a file it is going to store in working area.
+git status is the command
+
---
+++ stringutils.java
@@ -0,0 +1,5 @@
+There are three stages in git
+1 Working area
+2 Staging area
+3 Local repository
+
